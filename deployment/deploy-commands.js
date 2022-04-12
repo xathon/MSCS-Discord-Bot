1a114bff413904017c527931cfacd1ff56d56cf8
Alex Fuchs <github@xathon.de>
2022-04-12 05:20:43 +0200

    enable multiple worlds per guild
    also move away from application commands to guild commands

--- deployment/deploy-commands.js
+++ deployment/deploy-commands.js
@@ -6,19 +6,52 @@ const env  = require("../config.js");
 const { getEnabledCategories } = require('trace_events');
 
 
-const commands = [];
+let commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`../commands/${file}`);
+	const commandFile = require(`../commands/${file}`);
     console.log("Found " + file);
-	commands.push(command.data.toJSON());
+	commands.push(commandFile.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(env.token);
+let commands_mod;
 
-rest.put(Routes.applicationCommands(env.client_id), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+for (let guild of env.guilds) {
+	if(guild.worlds.length === 1) { //only one world, so no need for options
+		commands_mod = commands; //don't modify the original array
+	} else { //more than one world, so add options object
+		let worldSelector = {};
+		worldSelector.autocomplete = undefined;
+		worldSelector.choices = [];
+		for(let world of guild.worlds) {
+			worldSelector.choices.push({ name: world, value: world });
+		}
+		worldSelector.description = 'Choose the world to run this command on';
+		worldSelector.name = 'world';
+		worldSelector.required = true;
+		worldSelector.type = 3;
 
-    
+		commands_mod = structuredClone(commands); // deep copy commands so we can edit it without modifying the original
+
+		for (let command of commands_mod) {
+			if(command.name !== 'help' &&
+			   command.name !== 'lock' &&
+			   command.name !== 'ping') {
+				command.options?.unshift(worldSelector);
+			}
+		}
+		
+	}
+	if(process.argv.slice(2)[0] !== "dry") {
+
+		const rest = new REST({ version: '9' }).setToken(env.token);
+	
+		rest.put(Routes.applicationGuildCommands(env.client_id,guild.guildID), { body: commands_mod })
+			.then(() => console.log(`Successfully registered guild commands for ${guild.guildName}.`))
+			.catch(console.error);
+	
+	} else {
+		console.log(commands_mod);
+	}
+}
